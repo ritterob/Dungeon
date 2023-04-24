f8eb2a2d502df0c77fd16fb95f221492e2ddaec0
Rob Ritter <ritterhaus@gmail.com>
2023-04-24 15:55:27 -0500

    Initialized Dungeon project.

---
+++ Dungeon/Dungeon.cs
@@ -0,0 +1,12 @@
+﻿namespace Dungeon {
+
+    internal class Dungeon {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+
+        }   // end method Main()
+
+    }   // end class Dungeon
+
+}   // end namespace Dungeon
