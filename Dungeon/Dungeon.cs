140bf30ca1fcc8d0cf47eb1151cefb6be19b4ee2
Rob Ritter <ritterhaus@gmail.com>
2023-04-28 07:25:29 -0500

    Housekeeping for VS 2022

--- Dungeon/Dungeon.cs
+++ Dungeon/Dungeon.cs
@@ -2,11 +2,11 @@
 
     internal class Dungeon {
 
-/*         static void Main(string[] args) {
+        static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
 
         }   // end method Main()
- */
+
     }   // end class Dungeon
 
 }   // end namespace Dungeon
